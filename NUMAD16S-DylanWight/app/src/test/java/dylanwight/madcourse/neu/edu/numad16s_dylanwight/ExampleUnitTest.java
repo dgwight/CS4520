201c13b711158b435192b25de439656f1bc73264
Dylan Wight <wightdylan@aol.com>
2016-01-15 12:51:35 -0500

    Project created

---
+++ NUMAD16S-DylanWight/app/src/test/java/dylanwight/madcourse/neu/edu/numad16s_dylanwight/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package dylanwight.madcourse.neu.edu.numad16s_dylanwight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
