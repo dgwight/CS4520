003a078b130ad259a49a431e23c81d414e59f252
kekg602 <kekg602@yahoo.com>
2016-04-19 08:35:18 -0400

    Adding in add button and overflow button to menu with actions

--- NUMAD16S-DylanWight/app/src/main/java/dylanwight/madcourse/neu/edu/numad16s_dylanwight/foodGrouper/FoodGrouperActivity.java
+++ NUMAD16S-DylanWight/app/src/main/java/dylanwight/madcourse/neu/edu/numad16s_dylanwight/foodGrouper/FoodGrouperActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import java.util.Date;
@@ -54,6 +56,34 @@ public class FoodGrouperActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         setFoodEntry();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.food_grouper_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // show settings
+            case R.id.action_settings:
+                return true;
+
+            // allow the user to add data
+            case R.id.action_add_food_items:
+                Intent intent = new Intent(this, AddFoodActivity.class);
+                this.startActivity(intent);
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     private void setFoodEntry() {
